c6f685bafbfdd412ec43f2be90b442371de87d93
Nicholas Blott <blottn@tcd.ie>
2018-09-28 13:52:07 +0100

    really basic lookup

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import qualified Data.Text.Lazy as T
 
+find :: Int -> T.Text
+find x | x == 1    = "hello world"
+       | otherwise = "not found"
 
 main = scotty 3000 $
-  get "/" $ do
-    html $ "hello world"
+  get "/get/:id" $ do
+    id <- param "id"
+    html $ find id
