76c70e7514b0855b2c14e3540b5b1f998a3b8a08
Nicholas Blott <blottn@tcd.ie>
2018-10-08 19:32:42 +0100

    encapsulate index in function

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,12 @@ find :: Int -> T.Text
 find x | x == 1    = "hello world"
        | otherwise = "not found"
 
+index :: ActionM()
+index = html "<h1> Welcome to parch! </h1>"
+
 main = scotty 3000 $ do
     get "/get/:id" $ do
       id <- param "id"
       text $ find id
 
-    get "/" $ do
-      text $ "oh hai"
+    get "/" $ index
