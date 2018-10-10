44e62c09ea17a996de5ed2034e18257f66698c3f
Nicholas Blott <blottn@tcd.ie>
2018-10-10 14:02:24 +0100

    stable?

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,11 @@
 module Main( main) where
 
 import Control.Monad.IO.Class
-
 import qualified Web.Scotty as S
 import Text.Blaze.Html
 import Text.Blaze.Html.Renderer.Text
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-
 import Data.String
 import Data.Text (unpack, pack, Text)
 import qualified Data.Text.IO as T
@@ -22,15 +20,42 @@ archive = "archive"
 
 index :: S.ActionM()
 index = S.html . renderHtml $ do
-          H.head $
-            H.title $ toHtml $ text "Parch"
+          H.head $ do
+            H.title "Parch"
           H.body $ do
-            H.h1 $ toHtml $ text "Parch blog site"
-            H.p $ toHtml $ text "here's a paragraph"
+            H.h1 "Parch blog site"
+            H.h2 "Contents"
+            H.ul $ do
+                H.li $ H.a ! A.href "/get/blog" $ "my link"
+                H.li $ "another asdf"
+
+loginPage :: Bool -> S.ActionM()
+loginPage True = S.html . renderHtml $ do
+                              H.head $ do
+                                H.title "Parch - login"
+                              H.body $ do
+                                H.p "Already logged in!"
+                                H.a ! A.href "/" $ "home"
+                                
+loginPage False = S.html "not logged in"
+
+auth :: Text -> Text -> S.ActionM()
+auth "admin" "pass" = do 
+                        S.text "success"
+                        c.makeSimpleCookie "auth" "y"
+auth _ _ = S.text "oh no"
 
 main = S.scotty 3000 $ do
     S.get "/" $ do
       index
+
+    S.get "/recents" $ do
+      S.html "TODO"
+
+    S.get "/login/:debug" $ do
+      debug <- S.param "debug"
+      loginPage debug
+
     S.get "/get/:id" $ do
       postid <- S.param "id"
       post <- liftIO $ do
@@ -38,3 +63,9 @@ main = S.scotty 3000 $ do
         post <- withCurrentDirectory (wd </> archive) $ readFile $ fromString postid
         return post
       S.html $ fromString post
+
+    S.get "/auth/:name/:pass" $ do
+      name <- S.param "name"
+      pass <- S.param "pass"
+      auth name pass
+
